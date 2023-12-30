8ed519013482e8f1ac739d94b426f84a1d34a27b
Slawomir Nowicki <slawomir_nowicki@wp.pl>
2023-12-30 14:24:25 +0100

    Zadanie domowe1 (instrukcje warunkowe)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Michał");
+let sredniaOcen;
+
+/* 
+Poniżej tego komentarza przypisz wartosc do zmiennej sredniaOcen tak aby wyswietlił się tekst 
+bez czerwonego paska. 
+*/
+
+
+if (sredniaOcen > 4) {
+  console.log("Dostajesz świadectwo z czerwonym paskiem");
+} else {
+  console.log("Dostajesz zwykłe świadectwo");
+}
