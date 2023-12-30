7d1d9fe61efbffb95813d74daeb53de439f0e82b
Wiki <nowicka.wiktoria2013@gmail.com>
2023-12-30 15:11:00 +0100

    Rozwiązanie zadania 1

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let sredniaOcen;
 Poniżej tego komentarza przypisz wartosc do zmiennej sredniaOcen tak aby wyswietlił się tekst 
 z czerwonym paskiem. 
 */
-
+sredniaOcen = 10;
 
 if (sredniaOcen > 4) {
-  console.log("Dostajesz świadectwo z czerwonym paskiem");
+    console.log("Dostajesz świadectwo z czerwonym paskiem");
 } else {
-  console.log("Dostajesz zwykłe świadectwo");
+    console.log("Dostajesz zwykłe świadectwo");
 }
